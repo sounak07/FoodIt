538fd0e566b3e063ea2e852d990f0af17a313831
sounak07 <sounakume@gmail.com>
2019-07-11 13:46:34 +0530

    BurgerBuilder.js tests written and passed success

--- src/containers/BurgerBuilder/BurgerBuilder.test.js
+++ src/containers/BurgerBuilder/BurgerBuilder.test.js
@@ -4,6 +4,8 @@ import Adapter from "enzyme-adapter-react-16";
 import { BurgerBuilder } from "./BurgerBuilder";
 import BuildControls from "../../components/Burger/BuildControls/BuildControls";
 import OrderSummery from "../../components/Burger/OrderSummery/OrderSummery";
+import Spinner from "../../components/UI/Spinner/Spinner";
+import Burger from "../../components/Burger/Burger";
 
 configure({ adapter: new Adapter() });
 
@@ -26,5 +28,15 @@ describe("<BurgerBuilder/>", () => {
       purchaseContinue: false
     });
     expect(wrapper.find(OrderSummery)).toHaveLength(1);
+  });
+
+  it("should show spinner if no ingredients", () => {
+    wrapper.setProps({ ingredients: null });
+    expect(wrapper.find(Spinner)).toHaveLength(2);
+  });
+
+  it("should show Burger no ingredients", () => {
+    wrapper.setProps({ ingredients: { salad: 1, meat: 1 } });
+    expect(wrapper.find(Burger)).toHaveLength(1);
   });
 });
